a5721778c4632ff669184b54a995ef7082b30810
qutroDev <qutropubg@gmail.com>
2023-02-05 13:48:42 +0200

    feat:add header

---
+++ pages/header/script.js
@@ -0,0 +1,7 @@
+function ImageChange() {
+    document.getElementById("flex__img").src="components/green_whatsapp.svg";
+}
+
+function ImageReset() {
+    document.getElementById("flex__img").src="components/Vector.svg"
+}
